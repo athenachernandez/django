11d0307d2ced5db921cb5ae424d621b1f483a7f9
Athena Hernandez <athena.hernandez.23@Bshops.com>
2021-04-15 08:39:50 -0700

    Blue / partially purple complete

---
+++ members/views.py
@@ -0,0 +1,54 @@
+# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+def index():
+    template = loader.get_template('members/index.html')
+
+    context = {
+        'email': email,
+        'loggedIn': loggedIn,
+        'user': request.user,
+    }
+    return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+    template_name = 'members/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'members/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'members/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'members/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('members:results', args=(question.id,)))
