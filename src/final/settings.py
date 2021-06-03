f2063127d8e5ae591c57b17a573612d652d79dd0
Athena Hernandez <athena.hernandez.23@Bshops.com>
2021-06-02 21:04:38 -0700

    Got user sign up working

--- src/final/settings.py
+++ src/final/settings.py
@@ -58,6 +58,12 @@ LOGIN_REDIRECT_URL = '/posts'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_UNIQUE = True
+# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
